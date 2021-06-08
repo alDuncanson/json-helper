aeab18b81ba1be3086db57613af77077b0dd7a22
Al Duncanson <alexjduncanson@gmail.com>
2021-06-08 17:38:00 -0400

    initial commit: - setup extension structure - register json-helper.alphabetize-json command - alphabetize json by first layer keys

---
+++ src/extension.ts
@@ -0,0 +1,29 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	const alphabetizeJsonCommand: vscode.Disposable = vscode.commands.registerTextEditorCommand(
+		'json-helper.alphabetize-json',
+		(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+
+			const textDocumentJson = JSON.parse(textEditor.document.getText());
+			const alphabetizedEntries = Object.entries(textDocumentJson).sort((firstKey, secondKey) => firstKey[0].toLowerCase() < secondKey[0].toLowerCase() ? -1 : 1);
+			const alphabetizedJson = Object.fromEntries(alphabetizedEntries);
+			const alphabetizedJsonString = JSON.stringify(alphabetizedJson);
+
+			const firstTextLine = textEditor.document.lineAt(0);
+			const lastTextLine = textEditor.document.lineAt(textEditor.document.lineCount - 1);
+			const firstToLastRange = new vscode.Range(firstTextLine.range.start, lastTextLine.range.end);
+
+			edit.replace(firstToLastRange, alphabetizedJsonString);
+		});
+
+	context.subscriptions.push(alphabetizeJsonCommand);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
